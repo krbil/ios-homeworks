5fa4f23a4d5beb8a14758b436ef758f908e0791f
Krylov Ilia <prostokrbil@gmail.com>
2022-05-10 21:11:06 +0300

    first initial commit
    Home work

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Илья Крылов on 11.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelDateOfBirthday: UILabel!
+    @IBOutlet weak var labelCity: UILabel!
+    @IBOutlet weak var textView: UITextView!
+}
